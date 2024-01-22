9c997cd6af2a2199f341934a95ce4ec52650132f
kathelan <mateuszdeptulski@gmail.com>
2024-01-22 22:42:25 +0100

    added register form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/LoginForm.css'
-import { FaUser, FaLock } from "react-icons/fa";
+import {FaUser, FaLock} from "react-icons/fa";
 
 
 export default function LoginForm() {
-    return (
-        <div className="wrapper">
-            <form action="">
+
+    const [isRegister, setIsRegister] = useState(false);
+
+    const renderLoginBody = () => {
+        return (
+            <>
                 <h1>Login</h1>
                 <div className="input-box">
                     <input type="text" placeholder="Username" required/>
-                    <FaUser className="icon" />
+                    <FaUser className="icon"/>
                 </div>
                 <div className="input-box">
-                    <input type="text" placeholder="Password" required/>
-                    <FaLock className="icon" />
+                    <input type="password" placeholder="Password" required/>
+                    <FaLock className="icon"/>
                 </div>
 
                 <div className="remember-forgot">
@@ -28,8 +31,51 @@ export default function LoginForm() {
                 <button type="submit">Login</button>
 
                 <div className="register-link">
-                    <p>Don't have an account? <a href="#">Register</a></p>
+                    <p>Don't have an account? <a href="#" onClick={(e) => {
+                        e.preventDefault();
+                        setIsRegister(true);
+                    }}>Register</a></p>
                 </div>
+            </>
+        )
+    };
+
+    const renderRegisterBody = () => {
+        return (
+            <>
+                <h1>Register</h1>
+                <div className="input-box">
+                    <input type="text" placeholder="Username" required/>
+                    <FaUser className="icon"/>
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Password" required/>
+                    <FaLock className="icon"/>
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Confirm Password" required/>
+                    <FaLock className="icon"/>
+                </div>
+                <button type="submit">Register</button>
+                <div className="register-link">
+                    <p>
+                        Already have an account?
+                        <a href="#" onClick={(e) => {
+                            e.preventDefault();
+                            setIsRegister(false);
+                        }}>
+                            Login
+                        </a>
+                    </p>
+                </div>
+            </>
+        )
+    };
+
+    return (
+        <div className="wrapper">
+            <form action="">
+                {isRegister ? renderRegisterBody() : renderLoginBody()}
             </form>
         </div>
 
